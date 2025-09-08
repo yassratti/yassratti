b63c32798301bda443b4caa5f5fcd4e65e6f86b7
Yassr atti <cloudec31@gmail.com>
2025-09-08 23:58:01 +0200

    first commit: first text to ship the page

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Yassr Atti",
+  description:
+    "Hi i'm yassr, would add more shi in the future, pretty lazy rn, thanks for reading this",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
