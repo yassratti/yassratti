7de64d734a33aa547379de2ae344dcf029981888
Yassr atti <cloudec31@gmail.com>
2025-09-09 00:17:45 +0200

    bun build issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-import Image from "next/image";
 import Link from "next/link";
 export default function Home() {
   return (
     <div className="h-screen w-screen p-5">
       <div className="text-center w-full h-full flex flex-col items-center justify-center">
         <h1 className="text-4xl sm:text-6xl font-bold">
-          Hi 👋 i'm yassr, would add more shi in the future, pretty lazy rn,
-          thanks for reading this, go back to{" "}
+          Hi 👋 im yassr, would add more shi in the future, pretty lazy rn,
+          thanks for reading this, go back to
           {
             <Link
               href={"https://www.x.com/yassratti"}
